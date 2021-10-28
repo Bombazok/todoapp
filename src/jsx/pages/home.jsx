8afae87fc9ccfe9766ab3fd98e60f5694a0a0aa8
Bombazok <sergeipiskarev@yandex.ru>
2021-10-28 16:07:38 +0300

    feat: create blank Home version

--- src/jsx/pages/home.jsx
+++ src/jsx/pages/home.jsx
@@ -1,9 +1,27 @@
-function Home() {
-	return (
-		<div className="home">
-			<div className="tasks"></div>
+import { useEffect, useState } from "react";
 
-			<div className="info"></div>
+import Tasks from "../components/tasks";
+import Info from "../components/info";
+
+function Home() {
+	const [currentTask, setCurrentTask] = useState(null);
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		// we should use fetch here in case of server hold our data
+		let storageData = window.localStorage.getItem("data");
+
+		if (storageData) {
+			setData(storageData);
+			setCurrentTask(JSON.parse(storageData).currentTask);
+		}
+	}, []);
+
+	return (
+		<div className="home jcsb-aic wh-full">
+			<Tasks />
+
+			<Info />
 		</div>
 	);
 }
